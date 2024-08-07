c6a0c0715a435aab1e83774d43c508bb168392ae
manurare <mreyarea@gmail.com>
2024-08-07 18:19:51 +0000

    typos in metrics

--- metrics.py
+++ metrics.py
@@ -11,17 +11,14 @@ from model.mesh_fusion.util import car2sph, sph2erp
 from model.utils.utils import pil_to_torch
 from model.utils.loss_utils import psnr, lpips_alex, ssim, delta_inlier_ratio, abs_rel_error, lin_rms_sq_error
 from scipy.ndimage import map_coordinates
-from tqdm import tqdm
+import tqdm
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--exp_renders_dir", required=True, default=None)
 parser.add_argument("--gt_renders_dir", required=True, default=None)
 args = parser.parse_args()
 
-settings_file = os.path.join(args.experiment, "settings.json")
-settings = json.load(open(settings_file, "r"))
-
-files = glob.glob(os.path.join(args.exp_renders_dir, "depth", "depth_*.npy"))
+files = glob.glob(os.path.join(args.gt_renders_dir, "*_rgb.png"))
 numfiles = len(files)
 
 pred_rgb_files_format = os.path.join(args.exp_renders_dir, "rgb", "rgb_{:04}.png")
@@ -31,9 +28,10 @@ gt_depth_files_format = os.path.join(args.gt_renders_dir, "{:05}_depth.dpt")
 
 metrics = {"psnr": [], "ssim": [], "lpips": [], "absrel": [], "rms": [], "delta1": [], "delta2": [], "delta3": []}
 
+lpips_alex = lpips_alex.to("cuda")
 with torch.no_grad():
     for i in tqdm.tqdm(range(numfiles)):
-        pred_rgb = np.array(Image.open(pred_rgb_files_format.format(i))) / 255.
+        pred_rgb = np.array(Image.open(pred_rgb_files_format.format(i+1))) / 255.
         gt_rgb = np.array(Image.open(gt_rgb_files_format.format(i))) / 255.
         pred_rgb = torch.from_numpy(pred_rgb).permute(2, 0, 1).type(torch.float32).contiguous().to("cuda")[None]
         gt_rgb = torch.from_numpy(gt_rgb).permute(2, 0, 1).type(torch.float32).contiguous().to("cuda")[None]
@@ -43,8 +41,8 @@ with torch.no_grad():
         lpips_ = lpips_alex(pred_rgb, gt_rgb).cpu().item()
 
         # Depth
-        pred_depth = np.load(pred_depth_files_format.format(i))
-        gt_depth = read_dpt(gt_depth_files_format.format(i))
+        pred_depth = np.load(pred_depth_files_format.format(i+1)).squeeze()
+        gt_depth = read_dpt(gt_depth_files_format.format(i)).squeeze()
 
         mask = gt_depth > 0
 
